1642f95116e402bcec0e007562166337bbafec71
dbasdanis <dbasdanis97@gmail.com>
2023-04-22 16:26:36 +0200

    fix(frontend): Author to BlogAuthor

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import BlogPost from '@/components/BlogPost'
-import Author from '@/components/Author'
+import BlogAuthor from '@/components/BlogAuthor'
 import PostsByTag from '@/components/PostsByTag'
 import AllPosts from '@/components/AllPosts'
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/author/:username', component: Author },
+    { path: '/author/:username', component: BlogAuthor },
     { path: '/post/:slug', component: BlogPost },
     { path: '/tag/:tag', component: PostsByTag },
     { path: '/', component: AllPosts },

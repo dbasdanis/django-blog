7c983aa4d886d66b242a2c49b7e2bfd901270385
dbasdanis <dbasdanis97@gmail.com>
2023-04-22 15:28:44 +0200

    feat(frontend):change main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
+
 import router from '@/router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')

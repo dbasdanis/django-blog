b6294e59831d90f01e58fd04ff3e2ee6b2b1f519
dbasdanis <dbasdanis97@gmail.com>
2023-04-22 22:57:58 +0200

    feat: config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import { createApollo} from './vue-apollo'
 
+const app = createApp(App);
 
-createApp(App).use(router).mount('#app')
+app.config.productionTip = false;
+
+app.use(router);
+app.provide('apollo', createApollo({
+    httpEndpoint: 'http://localhost:8000/graphql',
+    wsEndpoint: null,
+  }));
+app.mount('#app');

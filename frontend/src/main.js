b4877c7a585d4edf6bd47ca175e6b717fbaa9c3c
dbasdanis <dbasdanis97@gmail.com>
2023-04-22 18:22:39 +0200

    fix: main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,4 @@
-// import { createApp } from 'vue'
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-import router from '@/router'
-
-// createApp(App).use(router).mount('#app')
-
-new Vue({
-    router,
-    render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
